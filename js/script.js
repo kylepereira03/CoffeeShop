b12fbd40511cd1c4daa16a86c30f2f219f328fc9
kylepereira03 <kylepereira3107@gmail.com>
2024-08-01 16:27:30 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,31 +1,6 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
-let backToTopBtn = document.getElementById('back-to-top');
-
-window.onscroll = () => {
-    let top = window.scrollY;
-
-    sections.forEach(sec => {
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
-        }
-    });
-
-    if (top > 20) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
-};
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
@@ -37,9 +12,4 @@ document.querySelectorAll('.navbar a').forEach(link => {
         menuIcon.classList.remove('bx-x');
         navbar.classList.remove('active');
     }
-});
-
-backToTopBtn.addEventListener('click', function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
 });
